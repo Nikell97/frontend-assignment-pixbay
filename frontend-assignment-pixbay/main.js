540915605910855502f50d817ecde05ce0f8f114
Victor Sjöberg <vsjberg13@gmail.com>
2023-02-07 12:50:35 +0100

    API key med exemple

--- frontend-assignment-pixbay/main.js
+++ frontend-assignment-pixbay/main.js
@@ -1,2 +1,4 @@
 //API KEY
 //https://pixabay.com/api/?key=33444826-ce140ec6a98c30c48c958bb9d
+//Detta skrivs i slutet på URL för att få bilderna med dessa taggar:
+//&q=yellow+flower&impage_type=photo
