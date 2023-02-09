eea41a1dca65fd6cd2d0d95bd2a1b045ee916ffd
Nikell97 <nikellmattias@gmail.com>
2023-02-09 08:24:39 +0100

    images from url

--- frontend-assignment-pixbay/main.js
+++ frontend-assignment-pixbay/main.js
@@ -11,14 +11,16 @@ form.onsubmit = async event => {
     let input = rawInput.replace(" ", "+");
     let color = form.colorList.value;
 
-    let url = 'https://pixabay.com/api/?key=33444826-ce140ec6a98c30c48c958bb9d&q=' + input + '&color=' + color + '&per_page=10';
+    let url = 'https://pixabay.com/api/?key=33444826-ce140ec6a98c30c48c958bb9d&q=' + input + '&colors=' + color + '&per_page=10';
     
     let response = await fetch(url);
     let json = await response.json();
+
+    
   
     for (let i = 0; i < json.hits.length; i++) {
-        let photoListItem = document.createElement('li');
-        photoListItem.textContent = json.hits[i].webformatURL;
+        let photoListItem = document.createElement('img');
+        photoListItem.src = json.hits[i].webformatURL;
         photoList.append(photoListItem);
     }
 };
