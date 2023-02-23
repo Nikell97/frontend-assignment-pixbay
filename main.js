5a720ac2c825bdc2c921c446dacbb7a63e9d66f7
Victor Sjöberg <vsjberg13@gmail.com>
2023-02-23 13:08:32 +0100

    än så länge

--- main.js
+++ main.js
@@ -7,69 +7,85 @@ let photoList = document.querySelector('#photos');
 let previousPageButton = document.getElementById("previousPage");
 let nextPageButton = document.getElementById("nextPage");
 
-let pageMin = 0;
-let pageMax = 10;
+var pageMin = 0;
+var pageMax = 10;
 
-let numberOfPhotosToPull = 50;
-let numberOfPhotos = 0;
+var photoPage = 1
+var numberOfPhotosToPull = 10;
+var numberOfPhotos = 0;
 
-let json = null;
+var json = null;
+var rawInput = null;
+var input = null;
+var rawColor = null;
+var color = null;
 
 form.onsubmit = async event => {
     event.preventDefault();
 
-    let rawInput = form.searchTerm.value;
-    let input = rawInput.replace(" ", "+");
-    let rawColor = form.colorList.value;
-    let color = rawColor.toLowerCase();
+    rawInput = form.searchTerm.value;
+    input = rawInput.replace(" ", "+");
+    rawColor = form.colorList.value;
+    color = rawColor.toLowerCase();
 
-    let url = 'https://pixabay.com/api/?key=33444826-ce140ec6a98c30c48c958bb9d&q=' + input + '&colors=' + color + '&per_page=' + numberOfPhotosToPull;
+    json = await apiCall();
+    numberOfPhotos = json.hits.length;
+
+    updateButtons(pageMin, pageMax, numberOfPhotos);
+    postPhotos(pageMin, pageMax, json);
+}
+
+async function apiCall() {
+    let url = 'https://pixabay.com/api/?key=33444826-ce140ec6a98c30c48c958bb9d&q=' + input + '&colors=' + color + '&per_page=' + numberOfPhotosToPull + '&page=' + photoPage;
     
     let response = await fetch(url);
     json = await response.json();
 
-    numberOfPhotos = json.hits.length;
-    //These exists so that you cant be on a page that dont exists when searching for new pictures.
-    pageMin = 0;
-    pageMax = 10;
-    updateButtons(pageMin, pageMax, numberOfPhotos);
-    getPhotos(pageMin, pageMax, json);
+    return json;
 }
 
-previousPageButton.addEventListener("click", function()
+previousPageButton.addEventListener("click", async function()
 {    
     if (pageMin > 0)
     {
         //Clears the current photos.
         document.getElementById("photos").innerHTML = "";
 
-        pageMin -= 10;
+        photoPage -= 1;
+        json = await apiCall();
+
         pageMax -= 10;
-        getPhotos(pageMin, pageMax, json);
+        pageMin -= 10;
+
+        postPhotos(pageMin, pageMax, json);
 
         updateButtons(pageMin, pageMax, numberOfPhotos);
     }
 });
 
-nextPageButton.addEventListener("click", function()
+nextPageButton.addEventListener("click", async function()
 {    
-    if (pageMax < numberOfPhotos)
+    if (pageMax < numberOfPhotos) //These two are currently the same so nothing happens!
     {
         //Clears the current photos.
         document.getElementById("photos").innerHTML = "";
 
-        pageMin += 10;
+        photoPage += 1;
+        json = await apiCall();
+
         pageMax += 10;
-        getPhotos(pageMin, pageMax, json);
+        pageMin += 10;
+
+        postPhotos(pageMin, pageMax, json);
 
         updateButtons(pageMin, pageMax, numberOfPhotos);
     }
 });
 
-function getPhotos(pageMin, pageMax, json)
+function postPhotos(pageMin, pageMax, json)
 {
     document.getElementById("photos").innerHTML = "";
-    for (let i = pageMin; i < pageMax; i++) {
+    for (let i = 0; i < json.hits.length; i++) {
         if (i < numberOfPhotos)//To keep the for loop form posting photos which dont exsists.
         {
             let photoListItem = document.createElement('figure');
@@ -94,18 +110,18 @@ function updateButtons(pageMin, pageMax, numberOfPhotos)
     //Limits the buttons so "pageMin" cant go below 0 and pageMax cant go beyond "numberOfPhotos".
     if (pageMin >= 10)
     {
-        previousPageButton.disabled = false;
+        //previousPageButton.disabled = false;
     }
     else
     {
-        previousPageButton.disabled = true;
+        //previousPageButton.disabled = true;
     }
     if (pageMax >= numberOfPhotos)
     {
-        nextPageButton.disabled = true;
+        //nextPageButton.disabled = true;
     }
     else
     {
-        nextPageButton.disabled = false
+        //nextPageButton.disabled = false
     }
 }
